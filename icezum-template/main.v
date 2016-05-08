96d55d3c1df7bfb28c525f9f7ef2ac74e7d984eb
obijuan <juan@iearobotics.com>
2016-05-08 10:49:10 +0200

    Icezum project templated added

---
+++ icezum-template/main.v
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------
+//-- Verilog template
+//-- Top entity
+//-- Board: icezum
+//------------------------------------------------------------------
+`default_nettype none
+
+//-- Template for the top entity
+module top(output wire LED0);
+
+//-- Turn on the LED0 on the icezum
+assign LED0 = 1'b1;
+
+endmodule
