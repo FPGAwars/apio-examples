f77377c10994f500a8eae3ceb748aaa5e86ed178
obijuan <juan@iearobotics.com>
2016-05-08 12:48:22 +0200

    Example: Template for the go-board added

---
+++ go-board-template/main.v
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------
+//-- Verilog template
+//-- Top entity
+//-- Board: Nandland go-board
+//------------------------------------------------------------------
+`default_nettype none
+
+//-- Template for the top entity
+module top(output wire LED1);
+
+//-- Turn on the D1 (red led) on the icestick
+assign LED1 = 1'b1;
+
+endmodule
