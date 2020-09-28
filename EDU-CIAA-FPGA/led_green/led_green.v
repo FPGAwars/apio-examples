4b2ec546999d8e1fa93443c7c210357f28029e68
Obijuan <juan@iearobotics.com>
2020-09-28 18:57:58 +0200

    EDU-CIAA-FPGA: Hello world example added

---
+++ EDU-CIAA-FPGA/led_green/led_green.v
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------
+//-- Hello world example for the EDU-CIAA-FPGA
+//-- Turn on the green LED
+//------------------------------------------------------------------
+module leds(output wire LEDG);
+ 
+  assign LEDG = 1'b1;
+  
+endmodule
+
