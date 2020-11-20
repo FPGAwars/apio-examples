7e85d0ca96af6403df5f7207e31ae5a29a79dfb0
Obijuan <obijuan.cube@gmail.com>
2020-11-20 17:59:43 +0100

    Blackice: Blinky added

---
+++ blackice/Blinky/Blinky.v
@@ -0,0 +1,23 @@
+//------------------------------------------------------------------
+//-- Blinking LED1 (Blue)
+//------------------------------------------------------------------
+
+module Test (
+  input CLK,    // 100MHz clock
+  output LED1,  // Led to blink
+  output LED2, LED3, LED4  //-- Other LEDs: turned off
+);
+
+  reg [25:0] counter = 0;
+
+  always @(posedge CLK) 
+    counter <= counter + 1;
+
+  assign LED1 = counter[25];
+
+  //-- Turn off the other LEDs
+  assign {LED2, LED3, LED4} = 3'b0;
+
+endmodule
+
+
