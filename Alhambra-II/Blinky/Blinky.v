a6df71ddb2de9d2683955431902167e6938bc5b1
Obijuan <obijuan.cube@gmail.com>
2020-11-20 08:09:32 +0100

    Blinky: Alhambra-II, Go-board: LEDs turned off

--- Alhambra-II/Blinky/Blinky.v
+++ Alhambra-II/Blinky/Blinky.v
@@ -4,7 +4,8 @@
 
 module Test (
   input CLK,    // 12MHz clock
-  output LED7
+  output LED7,   // LED to blicnk
+  output LED6, LED5, LED4, LED3, LED2, LED1, LED0 //-- The rest of the LED (turned off)
 );
 
   reg [23:0] counter = 0;
@@ -13,6 +14,10 @@ module Test (
     counter <= counter + 1;
 
   assign LED7 = counter[23];
+
+  //-- Turn off the other LEDs
+  assign {LED6, LED5, LED4} = 3'b0;
+  assign {LED3, LED2, LED1, LED0} = 4'b0;
 
 endmodule
 
--- go-board/Blinky/Blinky.v
+++ go-board/Blinky/Blinky.v
@@ -4,7 +4,8 @@
 
 module Test (
   input CLK,    // 25MHz clock
-  output LED1
+  output LED1,  //-- LED to Blink
+  output LED2, LED3, LED4  //-- The other LEDs
 );
 
   reg [23:0] counter = 0;
@@ -13,6 +14,9 @@ module Test (
     counter <= counter + 1;
 
   assign LED1 = counter[23];
+
+  //-- Turn off the other LEDs
+  assign {LED2, LED3, LED4} = 3'b0;
 
 endmodule
 
