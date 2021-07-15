9bc7419845b3a36598b732547d0c9303356c72cf
Obijuan <obijuan.cube@gmail.com>
2021-07-15 11:38:59 +0200

    Hello world example for the colorLight-5A-75E-V71

---
+++ ColoLight-5A-75E-V71_FT2232H/Ledon/ledon.v
@@ -0,0 +1,14 @@
+//--------------------------------------
+//-- Turning on the LED
+//--------------------------------------
+
+module top(
+    output led,   //-- LEDs
+);
+
+  // Turn on the led
+  // (output the bit 1 to the led)
+  assign led = 1'b1;
+
+endmodule
+
