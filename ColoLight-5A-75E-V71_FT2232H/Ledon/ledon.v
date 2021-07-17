ed89a08114b096e4be4044b98df18a50708332c4
obijuan <obijuan.cube@gmail.com>
2021-07-17 09:39:09 +0200

    ColorLight: Ledon: More Comments

--- ColoLight-5A-75E-V71_FT2232H/Ledon/ledon.v
+++ ColoLight-5A-75E-V71_FT2232H/Ledon/ledon.v
@@ -3,8 +3,13 @@
 //--------------------------------------
 
 module top(
-    output led,   //-- LEDs
+    output led,   //-- LED
 );
+
+  // -- The LED on the ColorLight-5A-75E works
+  // -- with inverse logic:
+  // -- 0: Turn on the LED 
+  // -- 1: Turn off the LED
 
   // Turn on the led
   // (output the bit 0 to the led)
