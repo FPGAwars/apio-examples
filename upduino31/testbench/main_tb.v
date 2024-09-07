49c2c08127dba00346b610733baa180f85e21049
Zapta <zapta@users.noreply.github.com>
2024-09-07 10:39:39 -0700

    Cleaned up the upduino31 testbench examples. Added a general apio testing macros that users can use for other boards. Down the road this testing file can move to a more visible place.

--- upduino31/testbench/main_tb.v
+++ upduino31/testbench/main_tb.v
@@ -1,31 +1,12 @@
-// An example of an automated test. Run 'apio test' to test and 'apio sim' to examine
-// the wave signals.
+// An example of an automated test. Run 'apio test' to test and
+// 'apio sim' to examine the wave signals.
+
+`include "apio_testing.vh"
 
 `timescale 10 ns / 1 ns
 
-`define DUMPSTR(x) `"x.vcd`"
-
-// A macro that check that given signal has given value. Normally it abors the simulation 
-// with an error, except when running with 'apio sim', in which case it prints an error
-// message and continues so we can examine the signal with gtkwave.
-`define EXPECT(signal, value) \
-    if (signal !== (value)) begin \
-        $display("ASSERTION FAILED in %m: signal != value  ('h%h)", (signal)); \
-        `ifndef INTERACTIVE_SIM \
-             $fatal; \
-        `endif \
-    end
-
-// A macro to wait for N clk positive edges, and then a little bit more for the last clock
-// to settle.
-`define CLK(n) \
-    begin \
-       repeat(n)  @ (posedge clk); \
-       #0.1; \
-    end
-
-// A macro to count N times.
-`define COUNT(n) \
+// A macro to increment the tested counter by n.
+`define INCREMENT(n) \
     begin \
         `CLK(1) \
         count_en = 1; \
@@ -34,32 +15,30 @@
         `CLK(1) \
     end
 
-
 module main_tb ();
+  // Declare the 'signal' name that is managed by apio_testings.vh.
+  `DEF_CLK();
 
-  always begin
-    #10 clk = ~clk;
-  end
-
-  // Inputs
-  reg clk = 0;
-  reg reset = 1;
-  reg count_en = 0;
+  // Inputs to the tested module.
+  reg reset;
+  reg count_en;
 
 
-  // Outputs
+  // Outputs of the tested module
   wire [3:0] digit_1;
   wire [3:0] digit_10;
   wire carry;
 
-  // Counts the number of clocks with carry=1 so far.
+  // A utility signal that indicates the number of clocks on which
+  // the carry output of the tested module was high.
   reg [5:0] carry_count = 0;
 
   always @(posedge clk) begin
     carry_count <= reset ? 0 : carry ? carry_count + 1 : carry_count;
   end
 
-  main main (
+  // The instance of the tested module.
+  main tested (
       .clk(clk),
       .reset(reset),
       .count_en(count_en),
@@ -68,48 +47,53 @@ module main_tb ();
       .carry(carry)
   );
 
+  // The test sequence.
   initial begin
-    $dumpfile(`DUMPSTR(`VCD_OUTPUT));
-    $dumpvars(0, main_tb);
+    `TEST_BEGIN(main_tb)
 
-    // Test reset.
+    // Reset the tested module.
+    count_en = 0;
+    reset = 1;
     `CLK(3)
     reset = 0;
-    `EXPECT(digit_10, 0) // count = 00
+    `EXPECT(digit_10, 0)  // count = 00
     `EXPECT(digit_1, 0)
+    `EXPECT(carry_count, 0)
+    `CLK(3);
+
+    // Count to 09.
+    `INCREMENT(9)
+    `EXPECT(digit_10, 0)
+    `EXPECT(digit_1, 9)
     `EXPECT(carry_count, 0)
 
     // Test transition from 09 to 10,
-    `CLK(3);
-    `COUNT(9)
-    `EXPECT(digit_10, 0)  // count = 09
-    `EXPECT(digit_1, 9)
-    `EXPECT(carry_count, 0)
-    `COUNT(1)
+    `INCREMENT(1)
     `EXPECT(digit_10, 1)  // count = 10
     `EXPECT(digit_1, 0)
     `EXPECT(carry_count, 0)
 
-    // Test transition from 99 to 00.
-    `COUNT(89)
-    `EXPECT(digit_10, 9) // count = 99
+    // Count to 99
+    `INCREMENT(89)
+    `EXPECT(digit_10, 9)  // count = 99
     `EXPECT(digit_1, 9)
     `EXPECT(carry_count, 0)
-    `COUNT(1)
-    `EXPECT(digit_10, 0) // count = 00
+
+    // Test transition from 99 to 00.
+    `INCREMENT(1)
+    `EXPECT(digit_10, 0)  // count = 00
     `EXPECT(digit_1, 0)
     `EXPECT(carry_count, 1)
 
-    // Count a few more
-    `COUNT(12)
-    `EXPECT(digit_10, 1) // count = 12
+    // Count to 12.
+    `INCREMENT(12)
+    `EXPECT(digit_10, 1)  // count = 12
     `EXPECT(digit_1, 2)
     `EXPECT(carry_count, 1)
 
     // All done.
     `CLK(10)
-    $display("End of simulation");
-    $finish;
+    `TEST_END()
   end
 
 endmodule
