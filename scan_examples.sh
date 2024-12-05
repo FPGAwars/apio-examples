183c76027bd7fe805f77ef414a865730c796294e
Zapta <zapta@users.noreply.github.com>
2024-12-05 09:36:44 -0800

    Now generating also reduced board and examples reports, with issues only.

--- scan_examples.sh
+++ scan_examples.sh
@@ -2,7 +2,8 @@
 
 # A shell script that scans the examples for various issues.
 
-rm -f _boards_report.csv _examples_report.csv
+rm -f _boards_full.csv _boards_issues_only.csv
+rm -f _examples_full.csv _examples_issues_only.csv
 
 python scripts/scan_examples.py
 
