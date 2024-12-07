f36ff762ac982baddc8f01ebed710d4ea7176f42
Zapta <zapta@users.noreply.github.com>
2024-12-06 17:28:08 -0800

    Fixed an example and added a testbench.

--- examples/alchitry-cu/blinky/blinky.v
+++ examples/alchitry-cu/blinky/blinky.v
@@ -1,32 +1,31 @@
 //blink all 8 leds on Alchitry-Cu Board
 
-module blinky (
+module blinky #(
+    // Default blink 1Hz. Can override in a testbench.
+    parameter integer DELAY = 100000000 / 2
+) (
     //inputs
-    input clk, //100MHz
+    input clk,  //100MHz
 
     //outputs
-    output reg [0:7] led = 8'b11111111 //start with all on
+    output reg [7:0] led = 8'b11111111  //start with all on
 );
 
+  // Blink counter.
+  reg [26:0] count = 0;
 
-//initialize counter and blink delay
-integer count = 27'b0;
-integer delay = 27'b101111101011110000100000000  >> 1; //half a second
-
-//blink all leds
-always @ (posedge clk) begin   
-    if (count == delay) begin 
-        if (led == 8'b0) begin  //if off
-            led <= 8'b11111111; //turn on
-        end
-        else begin
-            led <= 8'b0;
-        end
-        count = 27'b0;  //reset count
+  // Blink the leds
+  always @(posedge clk) begin
+    if (count >= DELAY[26:0]) begin
+      if (led == 8'b0) begin  //if off
+        led <= 8'b11111111;  //turn all leds on
+      end else begin
+        led <= 8'b0;  // turn all leds off
+      end
+      count = 27'b0;  //reset count
+    end else begin
+      count = count + 1'b1;
     end
-    else begin
-        count = count + 1'b1;
-    end
-end
+  end
 
 endmodule
