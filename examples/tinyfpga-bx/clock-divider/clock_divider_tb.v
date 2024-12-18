1b468d206285c62e2f81f88f0f71b76c852dc509
Zapta <zapta@users.noreply.github.com>
2024-12-18 00:21:17 -0800

    Added an experimental system-verilog example. Also added to the example linter reporting of testbenches that don't have testbench.gtkw files for displaying apio sim results.

--- examples/tinyfpga-bx/clock-divider/clock_divider_tb.v
+++ examples/tinyfpga-bx/clock-divider/clock_divider_tb.v
@@ -4,28 +4,29 @@
 
 module clock_divider_tb;
 
-wire clock_div4_0;
-wire clock_div4_90;
+  wire clock_div4_0;
+  wire clock_div4_90;
 
-reg clk = 0;
+  reg  clk = 0;
 
-clock_divider cdiv (.clock_in(clk),
-	.clock_div4_0(clock_div4_0),
-	.clock_div4_90(clock_div4_90));
+  clock_divider cdiv (
+      .clock_in(clk),
+      .clock_div4_0(clock_div4_0),
+      .clock_div4_90(clock_div4_90)
+  );
 
-initial begin
+  initial begin
     //-- Dump vars to the .vcd output file
-	$dumpvars;
-	#1000
-	$finish;
-end
+    $dumpvars(0, clock_divider_tb);
+    #1000 $finish;
+  end
 
-always begin
-	clk = 1'b1;
-	#2.5;
+  always begin
+    clk = 1'b1;
+    #2.5;
 
-	clk = 1'b0;
-	#2.5;
-end
+    clk = 1'b0;
+    #2.5;
+  end
 
 endmodule
