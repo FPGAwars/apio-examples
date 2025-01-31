bece1f6d975d9c414f1ab7f2e13413d70c0778ae
Zapta <zapta@users.noreply.github.com>
2025-01-30 17:04:43 -0800

    Added system verilog examples.

---
+++ examples/colorlight-5a-75b-v8/ledon-sv/ledon.sv
@@ -0,0 +1,19 @@
+//--------------------------------------
+//-- Turning on the LED
+//--------------------------------------
+
+module ledon (
+    output logic led  //-- LED
+);
+
+  // -- The LED on the ColorLight-5A-75E works
+  // -- with inverse logic:
+  // -- 0: Turn on the LED 
+  // -- 1: Turn off the LED
+
+  // Turn on the led
+  // (output the bit 0 to the led)
+  assign led = 1'b0;
+
+endmodule
+
