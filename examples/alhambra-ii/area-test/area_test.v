b6887d936b388904e186146fdc94a46372a0b8ab
Carlos Venegas <carlos@magnitude.es>
2025-06-30 00:20:34 +0200

    Fix verilator errors for area_test example

--- examples/alhambra-ii/area-test/area_test.v
+++ examples/alhambra-ii/area-test/area_test.v
@@ -15,7 +15,7 @@
  * - LED assignment: Final result displayed on 8 LEDs
  * 
  * Resource utilization estimate:
- * - ~98% of LUTs (multipliers + combinational logic)
+ * - ~60% of LUTs (multipliers + combinational logic)
  * - ~100% of BRAMs (32 of 32 available - all blocks used)
  * - ~30% of flip-flops (registers + counters)
  * 
@@ -152,7 +152,6 @@ module area_test (
         end
     endgenerate
     
-    // Remove old bram_filler as we now use all BRAMs directly
     
     // BRAM data processing - XOR all 32 BRAM outputs to ensure usage
     wire [31:0] all_bram_combined;
@@ -367,15 +366,16 @@ module area_test (
     assign LED0 = mult_rotating_bits[0];
 
 endmodule
+
 /*
  * Copyright 2020 Brian O'Dell
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -384,28 +384,27 @@ endmodule
  *
  */
 
-module bram (
-    input clk, 
-    input [BRAM_ADDR_WIDTH-1:0] addr, 
+module bram #(
+    parameter BRAM_ADDR_WIDTH = 9,
+    parameter BRAM_DATA_WIDTH = 8
+) (
+    input clk,
+    input [BRAM_ADDR_WIDTH-1:0] addr,
     input cs_n,
-    input wr_n, 
+    input wr_n,
     input rd_n,
     input [BRAM_DATA_WIDTH-1:0] bram_data_in,
     output reg [BRAM_DATA_WIDTH-1:0] bram_data_out
 );
-
-    parameter BRAM_ADDR_WIDTH = 9;
-    parameter BRAM_DATA_WIDTH = 8;
 
     reg [BRAM_DATA_WIDTH-1:0] mem [(1<<BRAM_ADDR_WIDTH)-1:0];
 
     always @(posedge clk)
         if (cs_n == 1'b0) begin
             begin
-                if (wr_n == 1'b0) mem[(addr)] <= bram_data_in;
+                if (wr_n == 1'b0) mem[addr] <= bram_data_in;
                 if (rd_n == 1'b0) bram_data_out <= mem[addr];
             end
         end
-    endmodule
 
-
+endmodule
